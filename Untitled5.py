f47f7f8b9062f7cde7295c0f92e9fb815811ba1f
atul611 <86343002+atul611@users.noreply.github.com>
2022-07-14 13:10:41 +0530

    Add files via upload

---
+++ Untitled5.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #### Spam and HAM is the major problem for the current Senerio so we are here to predict what kind of text messages will be Spam and vice versa. in this case we will go with the Two most popular library that is MultiNomial (Bayes Theorm) And Logistic Regression model  
+# #### Imoporting the Necessary Library 
+
+# # NLP model to taking care of Text Messages 
+
+# # Building The ML Model 
+
+# # Let's Try Predoicting with the Naive Bayes Algo
+
+# # We can say that Naive Bayes has better prediction than Logistic Regression Logistic Regression Prediction model = 0.9838565022421525 And Naive Bayes = 0.9847533632286996
+
+# ### Let's Visualize the Confusion Matrix
